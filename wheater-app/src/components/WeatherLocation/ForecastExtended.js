61e54bc8d62374eeaad7e1e41610267cc4c33b7a
Jose <antares069@gmail.com>
2019-10-14 18:16:16 -0500

    ok

---
+++ wheater-app/src/components/WeatherLocation/ForecastExtended.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class ForecastExtended extends Component{
+    render(){
+        return (<div>Pronostico extendido</div>);
+    }
+}
+
+export default ForecastExtended;
