4966e28d73290f29f9b0572107de730c9597e2a9
Jose <antares069@gmail.com>
2019-10-16 20:19:42 -0500

    Estilo

--- wheater-app/src/components/WeatherLocation/ForecastExtended.js
+++ wheater-app/src/components/WeatherLocation/ForecastExtended.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import './styles.css';
 
 class ForecastExtended extends Component{
     render(){
         const {city}=this.props;
-        return (<div>Pronostico para {city}</div>);
+        return (
+        <div>
+            <h2 className='forecast-tittle'>
+                Pronóstico para {city}
+            </h2>
+        </div>
+        );
     }
 }
 
