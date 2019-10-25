7bd10271485e5348eee7ba64c39d9438b7313a7a
Jose <antares069@gmail.com>
2019-10-25 18:22:03 -0500

    reducer opt

--- wheater-app/src/reducers/city.js
+++ wheater-app/src/reducers/city.js
@@ -4,7 +4,6 @@ export const city=(state,action) =>{
     switch (action.type) {
         case SET_CITY:
                 return { ...state, city:action.value}
-            break;
     
         default:
             break;
